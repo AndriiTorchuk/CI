bb8fce773215f1a1dedafc3b35f9524e944332de
AndriiTorchuk <andriytv86@gmail.com>
2017-08-07 22:37:27 +0200

    ex4, ex5

---
+++ ex4.py
@@ -0,0 +1,16 @@
+"""
+Exercise 4
+"""
+
+from ex2 import *
+import unittest
+class TestCalculate(unittest.TestCase):
+    def setUp(self):
+        self.calc = calculate()
+    def test_add_method_returns_correct_result(self):
+        self.assertEqual(4, self.calc.add(2,2))
+    def test_add_method_raises_typeerror_if_not_ints(self):
+        self.assertRaises(TypeError, self.calc.add, "Hello", "World")
+
+if __name__ =='__main__':
+    unittest.main()
---
+++ ex5.py
@@ -0,0 +1,12 @@
+"""
+Exercise 5
+"""
+
+import unittest
+
+class testt(unittest.TestCase):
+    def test_equ(self):
+        self.assertEqual(1, 1)
+
+unittest.main()
+
