6c228c968df18b1f1bd0125cf612fd4207f07d6a
Gregory Reshetniak <gregory.reshetniak@nokia.com>
2017-08-13 19:39:11 +0200

    fixed coverage

--- ex1.py
+++ ex1.py
@@ -1,11 +1,12 @@
 """
 Exercise 1
 """
-def calc_plus(a, b):
-    return(a+b)
-def calc_minus(a, b):
-     return(a-b)
-def calc_multiply(a, b):
-    return(a*b)
-def calc_divide(a, b):
-     return(a/b)
+class Calculator():
+    def calc_plus(self,a, b):
+        return(a+b)
+    def calc_minus(self,a, b):
+        return(a-b)
+    def calc_multiply(self,a, b):
+        return(a*b)
+    def calc_divide(self,a, b):
+        return(a/b)
